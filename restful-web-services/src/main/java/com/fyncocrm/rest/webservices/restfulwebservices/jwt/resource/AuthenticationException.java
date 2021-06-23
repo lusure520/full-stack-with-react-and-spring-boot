a565986fcae696be87c2c2f2c3ced332bb144341
Yuanlu Deng <dengyuanlu@gmail.com>
2021-06-23 16:49:54 +1200

    frontend update

---
+++ restful-web-services/src/main/java/com/fyncocrm/rest/webservices/restfulwebservices/jwt/resource/AuthenticationException.java
@@ -0,0 +1,7 @@
+package com.fyncocrm.rest.webservices.restfulwebservices.jwt.resource;
+public class AuthenticationException extends RuntimeException {
+    public AuthenticationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
